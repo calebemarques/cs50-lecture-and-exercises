e8295579fc08de8883a99b3d829909e339b19e43
Calebe Marques <calebecarvalhomarques@gmail.com>
2026-01-23 16:07:54 -0300

    Add name input functionality to main module

--- views.py
+++ views.py
@@ -3,8 +3,10 @@ import numpy as np
 import csv
 from PIL import Image
 
+names = []
 
 def main():
+    catchname()
     with open("images.csv", "r") as file:
          reader = csv.DictReader(file)
          for row in reader:
@@ -15,5 +17,9 @@ def calculate_brightness(filename):
         brightness = np.mean(np.array(img.convert("L"))) / 255
     return brightness
 
+def catchname(name):
+    name = input("name: ")
+    names.append(name)
+    print(names)
 
 main()
