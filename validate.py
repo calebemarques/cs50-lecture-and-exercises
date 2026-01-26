33dfcee163ceec137eacec1ddadf5f9f69677ee3
Calebe Marques <calebecarvalhomarques@gmail.com>
2026-01-26 17:53:30 -0300

    `Added condition to check length of domain's top-level domain in email validation`

--- validate.py
+++ validate.py
@@ -3,7 +3,7 @@ email = input("Enter your email address: ").strip()
 
 username, domain = email.split("@")
 try:
-    if username and "." in domain:
+    if username and "." in domain and len(domain.split(".")[-1]) >= 2: 
         print("Valid email address.")
     else:
         print("Invalid email address: Username part is empty.")
