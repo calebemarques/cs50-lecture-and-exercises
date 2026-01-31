ca11e01c706321ed050e790cf0065e7b078b0d68
Calebe Marques <calebecarvalhomarques@gmail.com>
2026-01-31 19:28:42 -0300

    UPDATES :))

--- twitter.py
+++ twitter.py
@@ -3,7 +3,11 @@ import re
 
 # Prompt the user to enter a URL, then strip any leading or trailing whitespace from the input
 url = input("Enter URL: ").strip()
-
+# Use a regular expression to check if the URL is a valid Twitter URL and extract the username
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)$", url, re.IGNORECASE): # If the URL matches the Twitter pattern, extract and print the username
+     print(f"Username: {matches.group(1)}")
+else:# If the URL does not match, print an error message
+        print("Invalid Twitter URL")
 #re.sub(    # Pattern to match 'http://' or 'https://' at the beginning of the URL
 #    r"^https?://",)
 # re.removeprefix  # Remove the 'http://' or 'https://' prefix from the URL if it exists
