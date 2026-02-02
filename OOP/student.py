1c54d0e7615fb35c7f906737868a0074f08db0c3
Calebe Marques <calebecarvalhomarques@gmail.com>
2026-02-02 10:20:54 -0300

    updates

---
+++ OOP/student.py
@@ -0,0 +1,16 @@
+def  main():
+    student = get_students()
+    print(f"{student[0]} from {student[1]}")
+
+
+def get_students():
+   student ={}
+   student["name"] = input("Name: ")
+   student["house"] = input("House: ")
+   return student   
+
+
+
+
+if __name__ == "__main__":    
+    main()
